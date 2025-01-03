2e3a24b4df7d2bf8861c886d561e08217e4251a7
agrxim <sagrim510@gmail.com>
2025-01-03 18:44:01 +0530

    image optimization

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { defineConfig } from 'vite';
+
+export default defineConfig(() => {
+  return {
+    plugins: [
+      ViteImageOptimizer({
+        jpg: {
+            
+            quality: 40,
+          },
+        png: {
+                quality: 40,
+            },
+      }),
+    ],
+  };
+});
