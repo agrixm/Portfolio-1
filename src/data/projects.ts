4579a4b14bcc1ccd5f873197c2132aaf32ec6243
agrxim <sagrim510@gmail.com>
2025-01-29 16:25:51 +0530

    projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,7 +2,7 @@ export const projects = [
   {
     id: 1,
     title: "My Portfolio UI/UX",
-    description: "A modern e-commerce platform built with React and Node.js",
+    description: "My Portfolio Designed in Framer ",
     image: "https://images.unsplash.com/photo-1557821552-17105176677c?auto=format&fit=crop&q=80",
     link: "https://agrim.framer.wiki/",
     tags: ["Framer"]
@@ -10,7 +10,7 @@ export const projects = [
   {
     id: 2,
     title: "Exclutch UI/UX",
-    description: "An AI-powered writing assistant that helps improve your content",
+    description: "A Notes Handling App Designed in Framer for University Students",
     image: "https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?auto=format&fit=crop&q=80",
     link: "https://edudoc.framer.wiki/",
     tags: ["Framer"]
@@ -18,7 +18,7 @@ export const projects = [
   {
     id: 3,
     title: "Instagram Profile Picture Downloader",
-    description: "A comprehensive design system for enterprise applications",
+    description: "A Python Script to Download Instagram Profile Picture",
     image: "https://images.unsplash.com/photo-1611262588024-d12430b98920?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     link: "https://github.com/agrixm/AGRIM-LEARNING/blob/master/Python%20Project.py",
     tags: ["Python"]
@@ -26,7 +26,7 @@ export const projects = [
   {
     id: 4,
     title: "Spotify Clone",
-    description: "A cross-platform mobile app for fitness tracking",
+    description: "A Spotify Clone Designed in HTML and CSS",
     image: "https://images.unsplash.com/photo-1614680376593-902f74cf0d41?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     link: "https://spotify-three-azure.vercel.app/",
     tags: ["HTML", "CSS"]
